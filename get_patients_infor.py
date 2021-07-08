1f5952972623f5fa1ecd2b1559c927c52b91d937
thuy4tbn99 <thuy4tbn99@Admin.localdomain>
2021-07-08 23:58:31 +0700

    end to end

--- get_patients_infor.py
+++ get_patients_infor.py
@@ -3,7 +3,15 @@ import os
 import re
 import pandas as pd
 import json
+from collections import OrderedDict
+from datetime import datetime
+from dateutil import parser
+from datetime import timedelta
+import categorizer
 
+
+#-----------------------------------------------------------------------------
+# congminh
 def getPersonalInformation(document):
     lst=[]
     for paragraph in document.paragraphs:
@@ -56,14 +64,14 @@ def cleanData(raw_dict):
     else:
         cleanedDict["SDT"] = ''
 
-    print('cleandata after', cleanedDict)
+    # print('cleandata after', cleanedDict)
     return cleanedDict
 
 
 def getPersonalInformationDetail(text):
     # print(text)
     regex_dict = {
-        "hoTen" : "(Bệnh nhân:.?([\w\sắằẳẵặăấầẩẫậâáàãảạđếềểễệêéèẻẽẹíìỉĩịốồổỗộôớờởỡợơóòõỏọứừửữựưúùủũụýỳỷỹỵẮẰẲẴẶĂẤẦẨẪẬÂÁÀÃẢẠĐẾỀỂỄỆÊÉÈẺẼẸÍÌỈĨỊỐỒỔỖỘÔỚỜỞỠỢƠÓÒÕỎỌỨỪỬỮỰƯÚÙỦŨỤÝỲỶỸỴ]){2,}([\t(,]){1})",
+        "hoTen" : "(Bệnh nhân:?.?([\w\sắằẳẵặăấầẩẫậâáàãảạđếềểễệêéèẻẽẹíìỉĩịốồổỗộôớờởỡợơóòõỏọứừửữựưúùủũụýỳỷỹỵẮẰẲẴẶĂẤẦẨẪẬÂÁÀÃẢẠĐẾỀỂỄỆÊÉÈẺẼẸÍÌỈĨỊỐỒỔỖỘÔỚỜỞỠỢƠÓÒÕỎỌỨỪỬỮỰƯÚÙỦŨỤÝỲỶỸỴ]){2,}([\t(,]){1})",
         "maBN":"(BN\s?[0-9]+)",
         "namSinh":"(sinh năm[:]*.? \d{4})",
         "gioiTinh":"(\s?(nam|nữ|NAM|NỮ|Nam|Nữ))",
@@ -108,7 +116,7 @@ def extract_sections(document_string, section):
 
 def find_job(section_string):
     """Return patient's job description from patient's info string"""
-    pattern = re.compile(r'(?<=Nghề nghiệp:)[^\n]*(?=\n)')
+    pattern = re.compile(r'(?<=Nghề nghiệp:)[^\n]*(?=\n)|(?<=Tên và địa chỉ nơi làm việc:)[^\n]*(?=\n)|(?<=Tên và địa chỉ làm việc)[^\n]*(?=\n)|(?<=Địa chỉ nơi ở và nơi làm)[^\n]*(?=\n)')
     job_search = re.search(pattern, section_string)
     if job_search:
         return job_search.group()
@@ -116,34 +124,12 @@ def find_job(section_string):
 def find_address(document_string):
     """Return patient's address from patient's info stirng"""
     address = ""
-    pattern = re.compile(r'(?<=Địa chỉ:)[^\n]+|(?<=Địa chỉ nơi ở:)[^\n]+|(?<=Địa chỉ tạm trú:)[^\n]+|(?<=Địa chỉ nơi ở hiện nay:)[^\n]+')
+    pattern = re.compile(r'(?<=Địa chỉ:)[^\n]+|(?<=Địa chỉ nơi ở:)[^\n]+|(?<=Địa chỉ tạm trú:)[^\n]+|(?<=Địa chỉ nơi ở hiện nay:)[^\n]+|(?<=Địa chỉ nơi)[^\n]+|(?<=Địa chỉ nơi ở và nơi làm)[^\n]+|(?<=Địa chỉ nhà:)[^\n]+')
     if re.search(re.compile(pattern),document_string):
         address = re.findall(pattern, document_string)[0]
     return address
 
-def split_address(address_string):
-    """Split patient's address into street, village, district, provine using regex"""
-    street = ""
-    village = ""
-    district = ""
-    provine = ""
-    district_n_provine = ""
-    village_pattern = re.compile(r'((?<=,)|(xã)|(phường))[^,]*, +((?=q)|(?=huyện)|(?=quận))')
-    district_pattern = re.compile(r'(quận|huyện|q).* +((?=tp)|(?=thành phố))')
-    village_search = re.search(village_pattern,address_string)
-    if village_search:
-        village = village_search.group()
-        street = address_string[:address_string.find(village)]
-        district_n_provine = address_string[address_string.find(village) + len(village):]
-        district_search = re.search(district_pattern, district_n_provine)
-        if district_search:
-            district = district_search.group()
-            provine = district_n_provine[district_n_provine.find(district)+len(district):]
-        else:
-            district = district_n_provine
-    return street, village, district, provine
 def split_address_normal(address_string):
-        """Split patient's address into street, village, district, provine using dictionary of HCMcity districts"""
         address_string = address_string.replace('.', '')
         districts = [ 'quận 11','q11', 'quận 12','q12' ,'quận 10','q10','quận 9','q9', 'quận 4','q4' ,   'quận 6','q6',
             'quận 2','q2','quận 5', 'q5', 'quận 7', 'q7','q3' ,'quận 3',  'quận 1', 'q1' ,
@@ -204,9 +190,9 @@ def split_address_normal(address_string):
                 break
         return street, village, district, provine
 
-def extract_patient_info(docx_file):
+def extract_patient_info(document_string):
     """"Return a json file which contains patient's information extracted from a word document"""
-    document_string = docx_to_string(docx_file)
+    # document_string = docx_to_string(docx_file)
     patient_info_section = extract_sections(document_string, 1)
     #get patient's job's description
     patient_job = find_job(patient_info_section)
@@ -214,9 +200,9 @@ def extract_patient_info(docx_file):
     patient_address = find_address(patient_info_section).lower()
     street, village, district, provine = split_address_normal(patient_address)
     output = {
-        "file_name": docx_file,
+        # "file_name": docx_file,
         "nghe_nghiep":patient_job,
-        "address": patient_address,
+        "dia chi": patient_address,
         "duong/thon/xom": street,
         "xa/phuong": village,
         "quan/huyen": district,
@@ -226,72 +212,96 @@ def extract_patient_info(docx_file):
 
 #------------------------------------------------------------
 # nghia
-from collections import OrderedDict
 
-#path docx file
-file_path = '/Users/user/Downloads/BÁO CÁO FILE WORD/BC CHUỖI CHƯA XÁC ĐỊNH/BN0000__MAI THỊ LOAN _26062021_VIỆN.docx'
-# file_path = '/Users/user/Downloads/BÁO CÁO FILE WORD/BN0000__HƯNG YÊN_TRẦN VĂN TĂNG_030721.docx'
-# file_path = '/Users/user/Downloads/BÁO CÁO FILE WORD/BC CHUỖI CHỢ BÌNH ĐIỀN - NHÓM 2+3/BN0000_ LÊ LÂM THỌ_ĐINH THỊ TRIỀU_24062021.docx'
+#path docx file (just for test)
+file_path = '/Users/user/Downloads/BÁO CÁO FILE WORD/BC 24 QUẬN HUYỆN TỪ 1-7/HUYỆN HÓC MÔN/BN0000_HM_LÊ THỊ HOÀNG_030721.docx'
 file_path = '/Users/user/Downloads/covid_path_split_files/arr_path_1.txt'
-# file_path = '/Users/user/Downloads/BÁO CÁO FILE WORD/BC 24 QUẬN HUYỆN TỪ 1-7/HUYỆN HÓC MÔN/BN0000_HM_LÊ THỊ HOÀNG_030721.docx'
-# file_path = '/Users/user/Downloads/BÁO CÁO FILE WORD/ BC CHUỖI VỰA VE CHAI/BN0000_ĐẶNG NGỌC PHƯƠNG_220621_NHÓM 4.docx'
-# file_path = '/Users/user/Downloads/BÁO CÁO FILE WORD/BC CHUỖI CHƯA XÁC ĐỊNH/BN00000_NGUYÊN THIÊN LỘC_26062021_BẰNG_N3.docx'
-#multi
-# file_path = '/Users/user/Downloads/BÁO CÁO FILE WORD/ BC CHUỖI VỰA VE CHAI/BN0000_BN0000_01_LƯƠNG THỊ THANH THUÝ_NGUYỄN THỊ LỆ THUỶ_01072021.docx'
+
+entry_dichte = False
+VN_regex_cap = "ẮẰẲẴẶĂẤẦẨẪẬÂÁÀÃẢẠĐẾỀỂỄỆÊÉÈẺẼẸÍÌỈĨỊỐỒỔỖỘÔỚỜỞỠỢƠÓÒÕỎỌỨỪỬỮỰƯÚÙỦŨỤÝỲỶỸỴ"
+VN_regex_norm = "áàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệóòỏõọôốồổỗộơớờởỡợíìỉĩịúùủũụưứừửữự"
+# date_regex = "[0-9]{1,2}/[0-9]{1,2}(?:\/[0-9]{4})?"
+date_regex = "[0-9]{1,2}/[0-1]{0,1}[0-9]{0,1}(?:\/[0-9]{4})?"
+prefix_date_regex = '(?:lấy[^.]*?'+date_regex+')|(?:[Ll]ần.*?'+date_regex+')|(?:'+date_regex+'[^\.]*?lấy mẫu)'
+BN_regex = "(?:BN ?\d+)|(?:BN (?:(?:[A-Z"+VN_regex_cap+"]{1,})\s?){2,5})|(?:BN (?:(?:[A-Z"+VN_regex_cap+"][a-z"+VN_regex_norm+"]{1,})\s?){2,5})"
 
 
-#create document object
-# document = Document(file_path)
-
-date_regex = "[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}"
-BN_regex = "(BN ?\d+)|(BN ([A-Z]+[a-áàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệóòỏõọôốồổỗộơớờởỡợíìỉĩịúùủũụưứừửữựýỳỷỹỵ]* *){1,4})"
 def extract_Ngay_duong_tinh(paragraph):
-    regex = "(Nhận thông tin lúc)"
-    regex = re.compile(regex)
+    regex = "(?:kết quả.*?dương tính[^\.]+?"+date_regex+")|(?:"+date_regex+"[^\./]+kết quả.*?dương tính)"
+    regex = re.compile(regex,flags=re.I)
     list_match = None
     if regex.search(paragraph.text):
         # list_match = [m for m in regex.findall(paragraph.text)]
         # print(list_match)
-        regex = re.compile(date_regex)
         list_match = regex.findall(paragraph.text)
-        if len(list_match) == 0:
-            regex = re.compile("ngày ?\d{1,2} ?tháng ?\d{1,2} ?năm ?\d{4}")
-            list_match = regex.findall(paragraph.text)
-            # print('hey',list_match)
-            if len(list_match) != 0:
-                list_match = list_match[0].split()
-                info = list_match[1]+'/'+list_match[3]+'/'+list_match[5]
-                return info
+        # print('ngay_duong_tinh',list_match)
+        for match in list_match:
+            arr = re.compile(date_regex).findall(match)
+        # list_match = list(OrderedDict.fromkeys(list_match))
+        return arr[-1]
+    else:
+        regex_ngay_lay_mau = re.compile(prefix_date_regex)
+        # regex_check = re.compile('')
+        if regex_ngay_lay_mau.search(paragraph.text):
+            arr = extract_Ngay_lay_mau(paragraph)
+            # print('arr',arr[-1])
+            if(len(arr[-1])<= 2):
+                time = datetime.strptime(arr[-1],'%d') + timedelta(days=1)
+                return time.strftime('%d')
+            elif (len(arr[-1])<=5):
+                time = datetime.strptime(arr[-1], '%d/%m') + timedelta(days=1)
+                return time.strftime('%d/%m')
+                # return time + datetime.timedelta(days=1)
+            else:
+                time = datetime.strptime(arr[-1], '%d/%m/%Y') + timedelta(days=1)
+                return time.strftime('%d/%m/%Y')
+
     return list_match
+
 def extract_Dich_te(paragraph):
-    regex = "([Dd]ịch [Tt]ễ)"
-    regex1 = "^\+ ?[^A-Za-z]"
+    regex = "[Dd]ịch [Tt]ễ:?.*"
     regex = re.compile(regex)
-    regex1 = re.compile(regex1)
-    if regex.search(paragraph.text):
-        info = paragraph.text.split(':,')
-        return info[-1].strip()
-    elif regex1.search(paragraph.text):
-        return paragraph.text
+    global entry_dichte
+    if (regex.search(paragraph.text) != None )| entry_dichte:
+        if re.compile('\n').search(paragraph.text):
+            # print("co dau xuong dong")
+            if re.compile('[+]').search(paragraph.text):
+                entry_dichte = True
+                if entry_dichte:
+                    return paragraph.text
+                else:
+                    return None
+            else:
+                entry_dichte = False
+                return paragraph.text
+        else:
+            if(paragraph.text.find(':')):
+                iter = paragraph.text.find(':')
+                return paragraph.text[iter+1:].strip()
     return None
+
 def extract_Ngay_lay_mau(paragraph):
-    regex = "([Dd]ương tính)"
-    regex = re.compile(regex)
-    list_match = []
+    # regex = "([Dd]ương tính)"
+    regex = re.compile(prefix_date_regex)
+    arr = []
     if regex.search(paragraph.text):
-        regex = re.compile(date_regex)
+        # regex = re.compile(prefix_date_regex)
         list_match = regex.findall(paragraph.text)
-        list_match = list(OrderedDict.fromkeys(list_match))
-        return list_match
+        # print('ngay_lay_mau',list_match)
+        for match in list_match:
+            arr.extend(re.compile(date_regex).findall(match))
+        return arr
     return None
+
 def extract_Tiep_xuc_ca_duong_tinh(paragraph):
-    regex = "([Dd]ương tính)"
+    regex = "(?:[Dd]ương tính)|(?:[Tt]heo [Dd]iện)"
     # ([Tt]iếp xúc)
     regex = re.compile(regex)
     if regex.search(paragraph.text):
-        regex = re.compile(BN_regex)
-        list_match = regex.findall(paragraph.text)
+        list_match = re.compile(BN_regex).findall(paragraph.text)
         # list_match = list(OrderedDict.fromkeys(list_match))
+        # print (paragraph.text)
+        # print('Tiep xuc',list_match)
         if len(list_match) == 0:
             return ['Chua ro nguon lay']
         else:
@@ -324,7 +334,7 @@ def single_patient(document):
             Dich_te.append(extract_Dich_te(paragraph))
         if extract_Ngay_lay_mau(paragraph) != None:
             # print(extract_Ngay_lay_mau(paragraph))
-            Ngay_lay_mau = extract_Ngay_lay_mau(paragraph)
+            Ngay_lay_mau.extend(extract_Ngay_lay_mau(paragraph))
         if extract_Tiep_xuc_ca_duong_tinh(paragraph) != None:
             # print(extract_Tiep_xuc_ca_duong_tinh(paragraph))
             Tiep_xuc_ca_duong_tinh = extract_Tiep_xuc_ca_duong_tinh(paragraph)
@@ -334,17 +344,16 @@ def single_patient(document):
             'Tiep_xuc_ca_duong_tinh':Tiep_xuc_ca_duong_tinh
               }
 
+# ----------------------------------------
+# input: directory path
+# output: file json chứa thông tin extract được
+def _get_patients_infor(directory_path):
+    categorizer_dict = categorizer.categorize(directory_path)
+    arr_path_multi = categorizer_dict['normal_single']
 
-if __name__ == '__main__':
-    # get path 1 patient
-    with open('./data/050721_arr_path_1.txt', 'r') as f:
-        arr_path_multi = f.readlines()
-        arr_path_multi = [x.replace('\n', '') for x in arr_path_multi]
-    import random
-    # idx_choice = random.randint(1,100)
     count = 0
     arr_patients_infor = []
-    for file_path in arr_path_multi[:]:
+    for file_path in arr_path_multi[:100]:
         try:
             print('file_path', file_path)
 
@@ -352,7 +361,9 @@ if __name__ == '__main__':
             document = Document(file_path)
             personal_infor = getPersonalInformation(document)
 
-            location_infor = extract_patient_info(file_path)
+            # get location
+            document_string = docx_to_string(file_path)
+            location_infor = extract_patient_info(document_string)
 
             # lich su
             history_move_infor = single_patient(document)
@@ -361,22 +372,31 @@ if __name__ == '__main__':
             patient_infor.update(location_infor)
             patient_infor.update(history_move_infor)
 
-                # for x in document.paragraphs:
-                #     print(x.text)
-
-            # print(patient_infor)
+            print(patient_infor)
             arr_patients_infor.append(patient_infor)
         except:
-        #     # for x in document.paragraphs:
-        #     #     print(x.text)
             count+=1
             print('---> error: ', file_path)
-    print(count, len(arr_patients_infor))
+    print('tổng lỗi:' ,count, '\ntổng file:',len(arr_patients_infor))
 
-    with open("patients_infor_050721.json", "w", encoding='utf-8') as write_file:
+    # save to json
+    path_save = 'patiens_infor_' + directory_path +'.json'
+    with open(path_save, "w", encoding='utf-8') as write_file:
         for patient_infor in arr_patients_infor:
             json.dump(patient_infor, write_file, ensure_ascii=False)
             write_file.write('\n')
     print("Done writing JSON serialized Unicode Data as-is into file")
+    return
+
+if __name__ == '__main__':
+
+    directory_path = '04-07-2021'
+    _get_patients_infor(directory_path)
+
+
+
+
+
+
 
 
